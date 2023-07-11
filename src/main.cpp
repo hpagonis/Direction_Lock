db47fefa242c8dd4bcf28a9dc753b4ab637bf0a9
Harris Pagonis <hpagonis@hotmail.com>
2023-07-11 20:53:08 +0300

    Added LED blinking to indicate wrong combination

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,40 @@
 #define EAST_PIN 3
 #define SOUTH_PIN 4
 #define WEST_PIN 5
-#define DEBOUNCE_INTERVAL 50
+#define RELAY_PIN 13
+#define LED_PIN 12
+#define DEBOUNCE_INTERVAL 200
+#define DIRECTIONS_NUMBER 6
+#define LED_BLINK_DELAY 500
 
 enum Direction {North, East, South, West};
-Direction sequence[5] = {North, North, South, East, East};
-Bounce btn_North = Bounce();
-Bounce btn_East = Bounce();
-Bounce btn_South = Bounce();
-Bounce btn_West = Bounce();
+Direction sequence[DIRECTIONS_NUMBER] = {South, East, North, South, West, North};
+uint8_t index = 0;
+Bounce2::Button btn_North = Bounce2::Button();
+Bounce2::Button btn_East = Bounce2::Button();
+Bounce2::Button btn_South = Bounce2::Button();
+Bounce2::Button btn_West = Bounce2::Button();
+
+void WrongDirection();
 
 void setup() {
   Serial.begin(115200);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   btn_North.attach(NORTH_PIN, INPUT);
   btn_North.interval(DEBOUNCE_INTERVAL);
+  btn_North.setPressedState(LOW);
   btn_East.attach(EAST_PIN, INPUT);
   btn_East.interval(DEBOUNCE_INTERVAL);
+  btn_East.setPressedState(LOW);
   btn_South.attach(SOUTH_PIN, INPUT);
   btn_South.interval(DEBOUNCE_INTERVAL);
+  btn_South.setPressedState(LOW);
   btn_West.attach(WEST_PIN, INPUT);
   btn_West.interval(DEBOUNCE_INTERVAL);
+  btn_West.setPressedState(LOW);
   Serial.println("Combination Lock initialized");
 }
 
@@ -33,7 +48,70 @@ void loop() {
   btn_South.update();
   btn_West.update();
 
-  if (btn_North.read() || btn_East.read() || btn_South.read() || btn_West.read()) {
-    Serial.println("Button pressed");
+  if (btn_North.pressed()) {
+    Serial.println("Button North pressed");
+    if (sequence[index] == North) {
+      Serial.println("Correct direction");
+      index++;
+    } else {
+      Serial.println("Wrong direction");
+      WrongDirection();
+      index = 0;
+    }
+    return;
+  }
+  if (btn_East.pressed()) {
+    Serial.println("Button East pressed");
+    if (sequence[index] == East) {
+      Serial.println("Correct direction");
+      index++;
+    } else {
+      Serial.println("Wrong direction");
+      WrongDirection();
+      index = 0;
+    }
+    return;
+  }
+  if (btn_South.pressed()) {
+    Serial.println("Button South pressed");
+    if (sequence[index] == South) {
+      Serial.println("Correct direction");
+      index++;
+    } else {
+      Serial.println("Wrong direction");
+      WrongDirection();
+      index = 0;
+    }
+    return;
+  }
+  if (btn_West.pressed()) {
+    Serial.println("Button West pressed");
+    if (sequence[index] == West) {
+      Serial.println("Correct direction");
+      index++;
+    } else {
+      Serial.println("Wrong direction");
+      WrongDirection();
+      index = 0;
+    }
+    return;
+  }
+  if (index == DIRECTIONS_NUMBER) {
+    Serial.println("Relay activated");
+    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(LED_PIN, HIGH);
+    delay(5000);
+    digitalWrite(RELAY_PIN, LOW);
+    index = 0;
+    Serial.println("Relay deactivated");
+  }
+}
+
+void WrongDirection() {
+  for (int i=0; i<3; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(LED_BLINK_DELAY);
+    digitalWrite(LED_PIN, LOW);
+    delay(LED_BLINK_DELAY);
   }
 }
