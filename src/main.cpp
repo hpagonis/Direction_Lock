5a55f2e66365438ce0e14ac0f72f3798c08d6e5a
Harris Pagonis <hpagonis@hotmail.com>
2023-07-12 20:00:33 +0300

    Changed logic to check input after the total number of directions is submitted

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,12 @@
 
 enum Direction {North, East, South, West};
 Direction sequence[DIRECTIONS_NUMBER] = {South, East, North, South, West, North};
+Direction input[DIRECTIONS_NUMBER];
 uint8_t index = 0;
 Bounce2::Button btn_North = Bounce2::Button();
 Bounce2::Button btn_East = Bounce2::Button();
 Bounce2::Button btn_South = Bounce2::Button();
 Bounce2::Button btn_West = Bounce2::Button();
-
-void WrongDirection();
 
 void setup() {
   Serial.begin(115200);
@@ -50,68 +49,48 @@ void loop() {
 
   if (btn_North.pressed()) {
     Serial.println("Button North pressed");
-    if (sequence[index] == North) {
-      Serial.println("Correct direction");
-      index++;
-    } else {
-      Serial.println("Wrong direction");
-      WrongDirection();
-      index = 0;
-    }
+    input[index] = North;
+    index++;
     return;
   }
   if (btn_East.pressed()) {
     Serial.println("Button East pressed");
-    if (sequence[index] == East) {
-      Serial.println("Correct direction");
-      index++;
-    } else {
-      Serial.println("Wrong direction");
-      WrongDirection();
-      index = 0;
-    }
+    input[index] = East;
+    index++;
     return;
   }
   if (btn_South.pressed()) {
     Serial.println("Button South pressed");
-    if (sequence[index] == South) {
-      Serial.println("Correct direction");
-      index++;
-    } else {
-      Serial.println("Wrong direction");
-      WrongDirection();
-      index = 0;
-    }
+    input[index] = South;
+    index++;
     return;
   }
   if (btn_West.pressed()) {
     Serial.println("Button West pressed");
-    if (sequence[index] == West) {
-      Serial.println("Correct direction");
-      index++;
-    } else {
-      Serial.println("Wrong direction");
-      WrongDirection();
-      index = 0;
-    }
+    input[index] = West;
+    index++;
     return;
   }
   if (index == DIRECTIONS_NUMBER) {
-    Serial.println("Relay activated");
-    digitalWrite(RELAY_PIN, HIGH);
-    digitalWrite(LED_PIN, HIGH);
-    delay(5000);
-    digitalWrite(RELAY_PIN, LOW);
+    if (memcmp(sequence, input, DIRECTIONS_NUMBER)) {
+      // Incorrect sequence
+      Serial.println("Wrong sequence");
+      for (int i=0; i<3; i++) {
+        digitalWrite(LED_PIN, HIGH);
+        delay(LED_BLINK_DELAY);
+        digitalWrite(LED_PIN, LOW);
+        delay(LED_BLINK_DELAY);
+      }
+    } else {
+      // Correct sequence
+      Serial.println("Relay activated");
+      digitalWrite(RELAY_PIN, HIGH);
+      digitalWrite(LED_PIN, HIGH);
+      delay(5000);
+      digitalWrite(RELAY_PIN, LOW);
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("Relay deactivated");
+    }
     index = 0;
-    Serial.println("Relay deactivated");
-  }
-}
-
-void WrongDirection() {
-  for (int i=0; i<3; i++) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(LED_BLINK_DELAY);
-    digitalWrite(LED_PIN, LOW);
-    delay(LED_BLINK_DELAY);
   }
 }
