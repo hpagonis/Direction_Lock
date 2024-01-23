c9767e6d12d24092c382cdfea8950f4aee0298ba
Harris Pagonis <hpagonis@hotmail.com>
2024-01-23 17:54:43 +0200

    Changed NSWE to Up,Down,Left,Right

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,74 @@
 #include <Arduino.h>
 #include "Bounce2.h"
 
-#define NORTH_PIN 2
-#define EAST_PIN 3
-#define SOUTH_PIN 4
-#define WEST_PIN 5
+#define UP_PIN 2
+#define RIGHT_PIN 3
+#define DOWN_PIN 4
+#define LEFT_PIN 5
 #define RELAY_PIN 13
 #define LED_PIN 12
 #define DEBOUNCE_INTERVAL 200
 #define DIRECTIONS_NUMBER 6
 #define LED_BLINK_DELAY 500
+#define BUTTON_NUM 4
 
-enum Direction {North, East, South, West};
-Direction sequence[DIRECTIONS_NUMBER] = {South, East, North, South, West, North};
+enum Direction {Up, Right, Down, Left};
+Direction sequence[DIRECTIONS_NUMBER] = {Down, Right, Up, Down, Left, Up};
 Direction input[DIRECTIONS_NUMBER];
-uint8_t index = 0;
-Bounce2::Button btn_North = Bounce2::Button();
-Bounce2::Button btn_East = Bounce2::Button();
-Bounce2::Button btn_South = Bounce2::Button();
-Bounce2::Button btn_West = Bounce2::Button();
+int8_t index = -1;
+Bounce2::Button btn_Up = Bounce2::Button();
+Bounce2::Button btn_Right = Bounce2::Button();
+Bounce2::Button btn_Down = Bounce2::Button();
+Bounce2::Button btn_Left = Bounce2::Button();
 
 void setup() {
-  Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
-  btn_North.attach(NORTH_PIN, INPUT);
-  btn_North.interval(DEBOUNCE_INTERVAL);
-  btn_North.setPressedState(LOW);
-  btn_East.attach(EAST_PIN, INPUT);
-  btn_East.interval(DEBOUNCE_INTERVAL);
-  btn_East.setPressedState(LOW);
-  btn_South.attach(SOUTH_PIN, INPUT);
-  btn_South.interval(DEBOUNCE_INTERVAL);
-  btn_South.setPressedState(LOW);
-  btn_West.attach(WEST_PIN, INPUT);
-  btn_West.interval(DEBOUNCE_INTERVAL);
-  btn_West.setPressedState(LOW);
-  Serial.println("Combination Lock initialized");
+  btn_Up.attach(UP_PIN, INPUT);
+  btn_Up.interval(DEBOUNCE_INTERVAL);
+  btn_Up.setPressedState(LOW);
+  btn_Right.attach(RIGHT_PIN, INPUT);
+  btn_Right.interval(DEBOUNCE_INTERVAL);
+  btn_Right.setPressedState(LOW);
+  btn_Down.attach(DOWN_PIN, INPUT);
+  btn_Down.interval(DEBOUNCE_INTERVAL);
+  btn_Down.setPressedState(LOW);
+  btn_Left.attach(LEFT_PIN, INPUT);
+  btn_Left.interval(DEBOUNCE_INTERVAL);
+  btn_Left.setPressedState(LOW);
 }
 
 void loop() {
-  btn_North.update();
-  btn_East.update();
-  btn_South.update();
-  btn_West.update();
+  btn_Up.update();
+  btn_Right.update();
+  btn_Down.update();
+  btn_Left.update();
 
-  if (btn_North.pressed()) {
-    Serial.println("Button North pressed");
-    input[index] = North;
+  if (btn_Up.pressed()) {
+    input[index] = Up;
     index++;
     return;
   }
-  if (btn_East.pressed()) {
-    Serial.println("Button East pressed");
-    input[index] = East;
+  if (btn_Right.pressed()) {
+    input[index] = Right;
     index++;
     return;
   }
-  if (btn_South.pressed()) {
-    Serial.println("Button South pressed");
-    input[index] = South;
+  if (btn_Down.pressed()) {
+    input[index] = Down;
     index++;
     return;
   }
-  if (btn_West.pressed()) {
-    Serial.println("Button West pressed");
-    input[index] = West;
+  if (btn_Left.pressed()) {
+    input[index] = Left;
     index++;
     return;
   }
   if (index == DIRECTIONS_NUMBER) {
     if (memcmp(sequence, input, DIRECTIONS_NUMBER)) {
       // Incorrect sequence
-      Serial.println("Wrong sequence");
       for (int i=0; i<3; i++) {
         digitalWrite(LED_PIN, HIGH);
         delay(LED_BLINK_DELAY);
@@ -83,13 +77,11 @@ void loop() {
       }
     } else {
       // Correct sequence
-      Serial.println("Relay activated");
       digitalWrite(RELAY_PIN, HIGH);
       digitalWrite(LED_PIN, HIGH);
       delay(5000);
       digitalWrite(RELAY_PIN, LOW);
       digitalWrite(LED_PIN, LOW);
-      Serial.println("Relay deactivated");
     }
     index = 0;
   }
